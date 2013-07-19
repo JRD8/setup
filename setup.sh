3abfe232af241460dd3a4707eda7792a41f4db20
JRD8 <jdforbew@gmail.com>
2013-07-19 07:35:41 -0400

    Added Gitbub and NPM installations

--- setup.sh
+++ setup.sh
@@ -49,5 +49,10 @@ sudo apt-get install -y python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install -y nodejs
+npm install
+
+# Install Github
+sudo apt-get install -y git core
+
 
 
